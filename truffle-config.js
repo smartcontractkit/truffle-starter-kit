95163502e6cfc98e02acac6b32b977a629ece812
Grant Steuart <gmsteuart@gmail.com>
2020-08-01 18:59:05 -0500

    revert temporary dry run fix

--- truffle-config.js
+++ truffle-config.js
@@ -17,9 +17,6 @@ module.exports = {
         return new HDWalletProvider(process.env.MNEMONIC, process.env.RPC_URL)
       },
       network_id: '*',
-      // Necessary due to https://github.com/trufflesuite/truffle/issues/1971
-      // Should be fixed in Truffle 5.0.17
-      skipDryRun: true,
     },
   },
   compilers: {
