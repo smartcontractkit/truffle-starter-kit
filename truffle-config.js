d6b93f196a7da6b5398fc200adbfa1b337b0c4fc
Grant Steuart <gmsteuart@gmail.com>
2020-08-01 21:06:33 -0500

    fix: migration on node v14.x.x

--- truffle-config.js
+++ truffle-config.js
@@ -17,6 +17,9 @@ module.exports = {
         return new HDWalletProvider(process.env.MNEMONIC, process.env.RPC_URL)
       },
       network_id: '*',
+      // ~~Necessary due to https://github.com/trufflesuite/truffle/issues/1971~~
+      // Necessary due to https://github.com/trufflesuite/truffle/issues/3008
+      skipDryRun: true,
     },
   },
   compilers: {
