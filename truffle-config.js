a159ac4ed3fa33cd78b58e04901b8b125466cc37
Ale <aadorian@gmail.com>
2020-12-26 22:30:56 -0300

    Update URL Binance Testnet

--- truffle-config.js
+++ truffle-config.js
@@ -8,7 +8,7 @@ module.exports = {
       network_id: '*',
     },
     testnet: {
-      provider: () => new HDWalletProvider(`${process.env.MNEMONIC}`, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      provider: () => new HDWalletProvider(`${process.env.MNEMONIC}`,'https://data-seed-prebsc-1-s1.binance.org:8545'),
       network_id: 97,
       confirmations: 10,
       timeoutBlocks: 200,
