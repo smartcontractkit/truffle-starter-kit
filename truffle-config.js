cd404b7ab464ec822b028a9a34b0bcbd1ab75fd8
Ale <aadorian@gmail.com>
2020-12-26 22:01:49 -0300

    Update binance testnet configuration

--- truffle-config.js
+++ truffle-config.js
@@ -7,6 +7,20 @@ module.exports = {
       port: 8545,
       network_id: '*',
     },
+    testnet: {
+      provider: () => new HDWalletProvider(`${process.env.MNEMONIC}`, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(`${process.env.MNEMONIC}`, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
     ganache: {
       host: '127.0.0.1',
       port: 7545,
@@ -21,6 +35,7 @@ module.exports = {
       // Necessary due to https://github.com/trufflesuite/truffle/issues/3008
       skipDryRun: true,
     },
+    
   },
   compilers: {
     solc: {
