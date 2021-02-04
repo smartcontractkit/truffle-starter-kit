cf368c0abc03bc8d6b1b55944a17712c8f23943c
Ale <aadorian@gmail.com>
2021-02-04 01:32:38 -0300

    add binance testnet-mainnet config in truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -12,6 +12,20 @@ module.exports = {
       port: 7545,
       network_id: '*',
     },
+    testnet: {
+      provider: () => new HDWalletProvider(`${process.env.MNEMONIC}`,'https://data-seed-prebsc-1-s1.binance.org:8545'),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(`${process.env.MNEMONIC}`, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
     live: {
       provider: () => {
         return new HDWalletProvider(process.env.MNEMONIC, process.env.RPC_URL)
