4a94a71394a8fa134fa0610f3aa7bca9fa9fda9b
Alex Roan <alex.roan@hotmail.com>
2021-02-06 22:40:04 +0000

    Proper linting

--- contracts/RandomNumberConsumer.sol
+++ contracts/RandomNumberConsumer.sol
@@ -17,11 +17,12 @@ contract RandomNumberConsumer is VRFConsumerBase {
      * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
      * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
      */
-    constructor(address _linkTokenAddress) 
+    constructor(address _linkTokenAddress)
+        public
         VRFConsumerBase(
             0xdD3782915140c8f3b190B5D67eAc6dc5760C46E9, // VRF Coordinator
             _linkTokenAddress  // LINK Token
-        ) public
+        )
     {
         keyHash = 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4;
         fee = 0.1 * 10 ** 18; // 0.1 LINK
