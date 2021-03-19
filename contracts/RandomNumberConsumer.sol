1b8e4ba7fc45bf3c21f7ece7d60ae77963c98792
PatrickAlphaC <54278053+PatrickAlphaC@users.noreply.github.com>
2021-03-18 21:04:19 -0400

    added events for logging random number

--- contracts/RandomNumberConsumer.sol
+++ contracts/RandomNumberConsumer.sol
@@ -8,7 +8,7 @@ contract RandomNumberConsumer is VRFConsumerBase {
     bytes32 internal keyHash;
     uint256 internal fee;
     uint256 public randomResult;
-    bytes32 public lastRequestId; 
+    event RequestedRandomness(bytes32 requestId);
     
     /**
      * Constructor inherits VRFConsumerBase
@@ -18,7 +18,8 @@ contract RandomNumberConsumer is VRFConsumerBase {
      * LINK token address:                0xa36085F69e2889c224210F603D836748e7dC0088
      * Key Hash: 0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4
      */
-    constructor(address _linkTokenAddress, bytes32 _keyHash, address _vrfCoordinatorAddress, uint256 _fee)
+    constructor(address _linkTokenAddress, bytes32 _keyHash, 
+    address _vrfCoordinatorAddress, uint256 _fee)
         public
         VRFConsumerBase(
             _vrfCoordinatorAddress, // VRF Coordinator
@@ -34,8 +35,9 @@ contract RandomNumberConsumer is VRFConsumerBase {
      * Requests randomness from a user-provided seed
      */
     function getRandomNumber(uint256 userProvidedSeed) public returns (bytes32 requestId) {
-        lastRequestId = requestRandomness(keyHash, fee, userProvidedSeed);
-        return lastRequestId;
+        requestId = requestRandomness(keyHash, fee, userProvidedSeed);
+        emit RequestedRandomness(requestId);
+        return requestId;
     }
 
     /** 
--- test/RandomNumberConsumer_test.js
+++ test/RandomNumberConsumer_test.js
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { expectRevert } = require('@openzeppelin/test-helpers')
 const { assert } = require('chai')
+const { expectRevert } = require('@openzeppelin/test-helpers')
 
 contract('RandomNumberConsumer', accounts => {
     const RandomNumberConsumer = artifacts.require('RandomNumberConsumer')
@@ -13,31 +12,26 @@ contract('RandomNumberConsumer', accounts => {
         beforeEach(async () => {
             keyhash = '0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4'
             fee = '1000000000000000000'
+            seed = 123
             link = await LinkToken.new({ from: defaultAccount })
             vrfCoordinatorMock = await VRFCoordinatorMock.new(link.address, { from: defaultAccount })
             randomNumberConsumer = await RandomNumberConsumer.new(link.address, keyhash, vrfCoordinatorMock.address, fee, { from: defaultAccount })
-            seed = 1
         })
-        it('reverts without LINK', async () => {
+        it('it revert without LINK', async () => {
             await expectRevert.unspecified(
-                randomNumberConsumer.getRandomNumber(seed, {
-                    from: defaultAccount,
-                }),
+                randomNumberConsumer.getRandomNumber(seed, { from: defaultAccount })
             )
         })
-        it('#can request and return a random number', async () => {
-            await link.transfer(randomNumberConsumer.address, web3.utils.toWei('1', 'ether'), {
-                from: defaultAccount,
-            })
-            let tx = await randomNumberConsumer.getRandomNumber(seed, { from: defaultAccount })
-            // just checking that is has logs
-            // console.log(tx.receipt.rawLogs)
-            assert.exists(tx.receipt.rawLogs, 'The tx hasnt been made!')
-            let requestId = await randomNumberConsumer.lastRequestId({ from: defaultAccount })
+        it('returns a random number with link', async () => {
+            await link.transfer(randomNumberConsumer.address, web3.utils.toWei('1', 'ether'), { from: defaultAccount })
+            let transaction = await randomNumberConsumer.getRandomNumber(seed, { from: defaultAccount })
+            assert.exists(transaction.receipt.rawLogs)
+            // This is the event that is emitted
+            let requestId = transaction.receipt.rawLogs[3].topics[0]
+            // let requestId = await randomNumberConsumer.lastRequestId({ from: defaultAccount })
             await vrfCoordinatorMock.callBackWithRandomness(requestId, '777', randomNumberConsumer.address, { from: defaultAccount })
             let randomNumber = await randomNumberConsumer.randomResult({ from: defaultAccount })
             assert.equal(randomNumber, 777)
         })
-
     })
 })
