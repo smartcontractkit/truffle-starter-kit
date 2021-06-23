aa607bee6885c52438e1271b71216b0c75bf40db
PatrickAlphaC <54278053+PatrickAlphaC@users.noreply.github.com>
2021-06-23 09:45:20 -0400

    Revert "Revert "updated for chainlink vrf api changes""
    This reverts commit 9e3cb44b4e3c1045590017ef1cc7c71f1670052f.

--- test/RandomNumberConsumer_test.js
+++ test/RandomNumberConsumer_test.js
@@ -6,25 +6,24 @@ contract('RandomNumberConsumer', accounts => {
     const VRFCoordinatorMock = artifacts.require('VRFCoordinatorMock')
     const { LinkToken } = require('@chainlink/contracts/truffle/v0.4/LinkToken')
     const defaultAccount = accounts[0]
-    let randomNumberConsumer, vrfCoordinatorMock, seed, link, keyhash, fee
+    let randomNumberConsumer, vrfCoordinatorMock, link, keyhash, fee
 
     describe('#request random number', () => {
         beforeEach(async () => {
             keyhash = '0x6c3699283bda56ad74f6b855546325b68d482e983852a7a82979cc4807b641f4'
             fee = '1000000000000000000'
-            seed = 123
             link = await LinkToken.new({ from: defaultAccount })
             vrfCoordinatorMock = await VRFCoordinatorMock.new(link.address, { from: defaultAccount })
             randomNumberConsumer = await RandomNumberConsumer.new(link.address, keyhash, vrfCoordinatorMock.address, fee, { from: defaultAccount })
         })
         it('it revert without LINK', async () => {
             await expectRevert.unspecified(
-                randomNumberConsumer.getRandomNumber(seed, { from: defaultAccount })
+                randomNumberConsumer.getRandomNumber({ from: defaultAccount })
             )
         })
         it('returns a random number with link', async () => {
             await link.transfer(randomNumberConsumer.address, web3.utils.toWei('1', 'ether'), { from: defaultAccount })
-            let transaction = await randomNumberConsumer.getRandomNumber(seed, { from: defaultAccount })
+            let transaction = await randomNumberConsumer.getRandomNumber({ from: defaultAccount })
             assert.exists(transaction.receipt.rawLogs)
             // This is the event that is emitted
             let requestId = transaction.receipt.rawLogs[3].topics[0]
